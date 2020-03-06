e9e3c972a658fb6a889dee4a2e06c241109c289c
Michael Ressler <36997670+mressler97@users.noreply.github.com>
2020-03-05 22:56:58 -0700

    Major overhaul of bluetooth functionality related code.

--- app/src/main/java/com/example/controlapp310/MainActivity.java
+++ app/src/main/java/com/example/controlapp310/MainActivity.java
@@ -1,43 +1,48 @@
 package com.example.controlapp310;
-// https://www.youtube.com/watch?v=cHj7kfLQyGo
 
-import android.annotation.SuppressLint;
+
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-    private final String DEVICE_ADDRESS = "20:15:11:23:93:85"; //MAC Address of Bluetooth Module
-    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
-    private BluetoothDevice device;
-    private BluetoothSocket socket;
-    private OutputStream outputStream;
+    //private final String DEVICE_ADDRESS = "98:D3:11:FC:48:82"; //MAC Address of Bluetooth Module
 
-    Button forward_btn, forward_left_btn, forward_right_btn, reverse_btn, reverse_left_btn, reverse_right_btn, bluetooth_connect_btn;
+    Button forward_btn, forward_left_btn, forward_right_btn, reverse_btn, reverse_left_btn, reverse_right_btn, bluetooth_connect_btn, disconnect;
+    String address = null;
+    String err = null;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket Socket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     String command; //string variable that will store value to be transmitted to the bluetooth module
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        //Intent newint = getIntent();
+        address = "98:D3:11:FC:48:82"; //receive the address of the bluetooth device
+
         setContentView(R.layout.activity_main);
 
-        //outputStream = new ByteArrayOutputStream(1024);   <- needed possibly
+        //outputStream = new ByteArrayOutputStream(1024);  // <- needed possibly
 
         //declaration of button variables
         forward_btn = (Button) findViewById(R.id.forward_btn);
@@ -45,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
         forward_right_btn = (Button) findViewById(R.id.forward_right_btn);
         reverse_btn = (Button) findViewById(R.id.reverse_btn);
         bluetooth_connect_btn = (Button) findViewById(R.id.bluetooth_connect_btn);
+        disconnect = (Button) findViewById(R.id.bluetooth_disconnect_btn);
 
+                                //calls class to connect
 
         //OnTouchListener code for the forward button (button long press)
         forward_btn.setOnTouchListener(new View.OnTouchListener() {
@@ -55,14 +62,15 @@ public class MainActivity extends AppCompatActivity {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
                 {
                     command = "1";
+                    System.out.println("forward!\n" + command.getBytes());
 
                     try
                     {
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
+                        Socket.getOutputStream().write(command.getBytes()); //transmits the value of command to the bluetooth module
                     }
                     catch (IOException e)
                     {
-                        e.printStackTrace();
+                        msg("error");
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
@@ -70,11 +78,11 @@ public class MainActivity extends AppCompatActivity {
                     command = "10";
                     try
                     {
-                        outputStream.write(command.getBytes());
+                        Socket.getOutputStream().write(command.getBytes());
                     }
                     catch(IOException e)
                     {
-                        e.printStackTrace();
+                        msg("error");
                     }
 
                 }
@@ -95,11 +103,11 @@ public class MainActivity extends AppCompatActivity {
 
                     try
                     {
-                        outputStream.write(command.getBytes());
+                        Socket.getOutputStream().write(command.getBytes());
                     }
                     catch (IOException e)
                     {
-                        e.printStackTrace();
+                        msg("error");
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -107,11 +115,11 @@ public class MainActivity extends AppCompatActivity {
                     command = "10";
                     try
                     {
-                        outputStream.write(command.getBytes());
+                        Socket.getOutputStream().write(command.getBytes());
                     }
                     catch(IOException e)
                     {
-
+                        msg("error");
                     }
 
                 }
@@ -130,11 +138,11 @@ public class MainActivity extends AppCompatActivity {
 
                     try
                     {
-                        outputStream.write(command.getBytes());
+                        Socket.getOutputStream().write(command.getBytes());
                     }
                     catch (IOException e)
                     {
-                        e.printStackTrace();
+                        msg("error");
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -142,11 +150,11 @@ public class MainActivity extends AppCompatActivity {
                     command = "10";
                     try
                     {
-                        outputStream.write(command.getBytes());
+                        Socket.getOutputStream().write(command.getBytes());
                     }
                     catch(IOException e)
                     {
-
+                        msg("error");
                     }
 
                 }
@@ -165,11 +173,11 @@ public class MainActivity extends AppCompatActivity {
 
                     try
                     {
-                        outputStream.write(command.getBytes());
+                        Socket.getOutputStream().write(command.getBytes());
                     }
                     catch (IOException e)
                     {
-                        e.printStackTrace();
+                        msg("error");
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -177,11 +185,11 @@ public class MainActivity extends AppCompatActivity {
                     command = "10";
                     try
                     {
-                        outputStream.write(command.getBytes());
+                        Socket.getOutputStream().write(command.getBytes());
                     }
                     catch(IOException e)
                     {
-                        e.printStackTrace();
+                        msg("error");
                     }
 
                 }
@@ -189,14 +197,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        disconnect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                try {
+                    Socket.close();
+                } catch (IOException e) {
+                    msg("error");
+                }
+
+            }
+        });
+
         //Button that connects the device to the bluetooth module when pressed
         bluetooth_connect_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if(BTinit())
+                bluetooth_connect_btn.setOnClickListener(this);
+                if(Socket == null)
                 {
-                    BTconnect();
+                    new ConnectBT().execute();
                 }
 
             }
@@ -204,87 +225,64 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Initializes bluetooth module
-    public boolean BTinit()
+
+    private void msg(String err)
     {
-        boolean found = false;
+        Toast.makeText(getApplicationContext(),err,Toast.LENGTH_LONG).show();
+    }
 
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if(bluetoothAdapter == null) //Checks if the device supports bluetooth
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute()
         {
-            Toast.makeText(getApplicationContext(), "Device doesn't support bluetooth", Toast.LENGTH_SHORT).show();
+            progress = ProgressDialog.show(MainActivity.this, "Connecting...", "Please wait!");  //show a progress dialog
         }
 
-        if(!bluetoothAdapter.isEnabled()) //Checks if bluetooth is enabled. If not, the program will ask permission from the user to enable it
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
         {
-            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableAdapter,0);
-
             try
             {
-                Thread.sleep(1000);
-            }
-            catch(InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
-
-        if(bondedDevices.isEmpty()) //Checks for paired bluetooth devices
-        {
-            Toast.makeText(getApplicationContext(), "Please pair the device first", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            for(BluetoothDevice iterator : bondedDevices)
-            {
-                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                if (Socket == null || !isBtConnected)
                 {
-                    device = iterator;
-                    found = true;
-                    break;
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    Socket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    Socket.connect();//start connection
                 }
             }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
         }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
 
-        return found;
+            if (!ConnectSuccess)
+            {
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            }
+            else
+            {
+                msg("Connected.");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
     }
 
-    public boolean BTconnect()
-    {
-        boolean connected = true;
 
-        try
-        {
-            socket = device.createRfcommSocketToServiceRecord(PORT_UUID); //Creates a socket to handle the outgoing connection
-            socket.connect();
 
-            Toast.makeText(getApplicationContext(),
-                    "Connection to bluetooth device successful", Toast.LENGTH_LONG).show();
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-            connected = false;
-        }
-
-        if(connected)
-        {
-            try
-            {
-                outputStream = socket.getOutputStream(); //gets the output stream of the socket
-            }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        return connected;
-    }
 
     @Override
     protected void onStart()
